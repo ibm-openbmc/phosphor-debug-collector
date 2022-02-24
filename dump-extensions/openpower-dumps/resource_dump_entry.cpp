9a843b3e7c1cbb38ddf09c7add2c0116ed227c90
Chirag Sharma <chirshar@in.ibm.com>
2022-02-24 04:53:46 -0600

    openPower: Removing serial files during delete operation
    Changes:
    -Deleting the serialization files of system and resource dumps,
    which are used to restore the dbus entries, when delete
    entry operation is called need to delete the files from the path,
    so that the entries don't get populate again.
    
    Tested:
    -Created resource and system dump, this operation creates serial
    files for respective dumps. After issuing delete operation, can see
    the files from serial path is getting deleted.
    
    Signed-off-by: Chirag Sharma <chirshar@in.ibm.com>
    Change-Id: I4ee7accda3eaf4a8c8641739153478ddd97f6c84

--- dump-extensions/openpower-dumps/resource_dump_entry.cpp
+++ dump-extensions/openpower-dumps/resource_dump_entry.cpp
@@ -75,9 +75,10 @@ void Entry::delete_()
     log<level::INFO>(fmt::format("Resource dump delete id({}) srcdumpid({})",
                                  dumpId, srcDumpID)
                          .c_str());
+    auto path = std::filesystem::path(RESOURCE_DUMP_SERIAL_PATH) /
+                std::to_string(dumpId);
 
     // Remove resource dump when host is up by using source dump id
-
     // which is present in resource dump entry dbus object as a property.
     if ((phosphor::dump::isHostRunning()) && (srcDumpID != INVALID_SOURCE_ID))
     {
@@ -98,6 +99,18 @@ void Entry::delete_()
 
     // Remove Dump entry D-bus object
     phosphor::dump::Entry::delete_();
+    try
+    {
+        std::filesystem::remove_all(path);
+    }
+    catch (const std::filesystem::filesystem_error& e)
+    {
+        // Log Error message and continue
+        log<level::ERR>(
+            fmt::format("Failed to delete dump file({}), errormsg({})",
+                        path.string(), e.what())
+                .c_str());
+    }
 
     // Log PEL for dump /offload
     auto dBus = sdbusplus::bus::new_default();
