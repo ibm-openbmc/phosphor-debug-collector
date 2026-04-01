0a92ccd225360a1bff64b8d822435f24471fbe04
Gopichand Paturi <gopichandpaturi@gmail.com>
2026-04-01 07:06:23 -0500

    openpower-dumps: Move opdump utility functions to utility file
    Change-Id: Ic0cef6073107c97fbe928101366395f4ef7bcc0d
    Signed-off-by: Gopichand Paturi <gopichandpaturi@gmail.com>

--- dump-extensions/openpower-dumps/dump_entry_factory.cpp
+++ dump-extensions/openpower-dumps/dump_entry_factory.cpp
@@ -199,7 +199,7 @@ std::unique_ptr<phosphor::dump::Entry> DumpEntryFactory::createEntry(
          (!dumpParams.vspString.has_value() || dumpParams.vspString->empty() ||
           toUpper(*dumpParams.vspString) == "SYSTEM"));
 
-    uint32_t dumpIdPrefix = getDumpIdPrefix(
+    uint32_t dumpIdPrefix = util::getDumpIdPrefix(
         createSystemDump ? OpDumpTypes::System : dumpParams.type);
 
     id |= dumpIdPrefix;
@@ -272,7 +272,7 @@ std::optional<std::unique_ptr<phosphor::dump::Entry>>
 
     for (const auto& entry : entries)
     {
-        if (getDumpTypeFromId(entry.second->getDumpId()) != dumpType)
+        if (util::getDumpTypeFromId(entry.second->getDumpId()) != dumpType)
         {
             continue;
         }
@@ -341,7 +341,7 @@ std::optional<std::unique_ptr<phosphor::dump::Entry>>
         return std::nullopt;
     }
 
-    id |= getDumpIdPrefix(dumpType);
+    id |= util::getDumpIdPrefix(dumpType);
     std::string idStr = std::format("{:08X}", id);
     auto objPath = std::filesystem::path(baseEntryPath) / idStr;
 
@@ -376,7 +376,7 @@ std::unique_ptr<phosphor::dump::Entry>
     DumpEntryFactory::createEntryWithDefaults(
         uint32_t id, const std::filesystem::path& objPath)
 {
-    auto type = getDumpTypeFromId(id);
+    auto type = util::getDumpTypeFromId(id);
 
     switch (type)
     {
